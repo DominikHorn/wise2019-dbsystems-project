673520bc95854fb18aaa3ce6bdea51b89634516e
Dominik Horn <dominik.horn@tum.de>
2019-11-13 23:46:36 +0100

    Refine and add missing entities to sql schema

--- implementation/postgres/setupPostgres.sql
+++ implementation/postgres/setupPostgres.sql
@@ -2,11 +2,9 @@ CREATE SCHEMA IF NOT EXISTS "landtagswahlen" AUTHORIZATION postgres;
 
 CREATE TABLE IF NOT EXISTS "landtagswahlen".parteien (
 	id int NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	"name" varchar(200) NOT NULL
+	"name" varchar(200) NOT NULL,
+	gruendungsDatum date NOT NULL
 );
-
--- TODO: Von Partei aufgestellte Liste mit Reihenfolge für 2. Kandidaten
--- TODO: Als view, Liste mit Reihenfolge nach Stimmauszählung (wichtig zur Sitzberechnung)
 
 CREATE TABLE IF NOT EXISTS "landtagswahlen".kandidaten (
 	id int NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -18,23 +16,56 @@ CREATE TABLE IF NOT EXISTS "landtagswahlen".kandidaten (
 
 CREATE TABLE IF NOT EXISTS "landtagswahlen".wahlen (
 	id int NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	datum date NOT NULL
+	wahldatum date NOT NULL
 );
 
--- Mangels nicht existierenden Daten werden Stimmbezirke nicht modeliert;
+CREATE TABLE IF NOT EXISTS "landtagswahlen".listen (
+	kandidat_id int NOT NULL, -- Partei ergibt sich durch Kandidat (TODO: stimmt diese Annahme?)
+	wahl_id int NOT NULL,
+	-- Da wir informatiker sind muss man sich hier auf einen standard einigen => Beginn bei 1 zu zählen
+	initialerListenplatz int CHECK (initialerListenplatz > 0), 	
+	FOREIGN KEY (kandidat_id) REFERENCES kandidaten(id),
+	FOREIGN KEY (wahl_id) REFERENCES wahlen(id),
+	PRIMARY KEY (kandidat_id, wahl_id)
+);
+
+-- Refresh am Ende der LW mit: REFRESH MATERIALIZED VIEW "landtagswahlen".finaleliste;
+DROP MATERIALIZED VIEW IF EXISTS "landtagswahlen".finaleliste;
+CREATE MATERIALIZED VIEW "landtagswahlen".finaleliste AS (
+	-- TODO: definiere query wenn klar wie sich das errechnet
+	SELECT *
+	FROM listen
+);
+
+
+-- Mangels nicht existierenden Daten werden Stimmbezirke nicht als relation repräsentiert;
 -- Somit entfaellt auch die Modelierung von Direktregionen
 
 -- Abbildung "Stimmkreis"-Entität
 CREATE TABLE IF NOT EXISTS "landtagswahlen".stimmkreise (
 	id int NOT NULL PRIMARY KEY, -- Schluessel_Nummer in csv
 	"name" varchar(80) NOT NULL,
+--	wahl_id int NOT NULL,
+--	anzahlWahlberechtigte int NOT NULL DEFAULT 0,
 	regierungsbezirk_id int,
-	FOREIGN KEY (regierungsbezirk_id) REFERENCES regierungsbezirke(id)
+	-- TODO: geographische angaben um zuordnen zu können?
+	FOREIGN KEY (regierungsbezirk_id) REFERENCES regierungsbezirke(id),
+--	FOREIGN KEY (wahl_id) REFERENCES wahlen(id)
 -- Unsupported in PostgreSQL
 --	CHECK (id NOT IN (
 --		SELECT id
 --		FROM regierungsbezirke
 --	))
+);
+
+-- TODO: wahl_id in stimmkreise aufnehmen? 2018 und 2013 gleiche Stimmkreise (-> nicht normalisiert?)!
+CREATE TABLE IF NOT EXISTS "landtagswahlen".stimmkreis_wahlberechtigte (
+	stimmkreis_id int NOT NULL,
+	wahl_id int NOT NULL,
+	anzahlWahlberechtigte int NOT NULL DEFAULT 0,
+	FOREIGN KEY (stimmkreis_id) REFERENCES stimmkreise(id),
+	FOREIGN KEY (wahl_id) REFERENCES wahlen(id),
+	PRIMARY KEY (stimmkreis_id, wahl_id)
 );
 
 -- Abbildung "Regierungsbezirk"-Entität
@@ -48,22 +79,34 @@ CREATE TABLE IF NOT EXISTS "landtagswahlen".regierungsbezirke (
 --	))
 );
 
+-- Berechnung der #Wahlberechtigten pro Regierungsbezirk statt Speicherung 
+CREATE OR REPLACE VIEW "landtagswahlen".regierungsbezirk_wahlberechtigte AS (
+	SELECT rb.id, rb."name", w.datum, sum(skw.anzahlWahlberechtigte)
+	FROM regierungsbezirke rb, wahlen w
+		JOIN stimmkreise sk ON sk.regierungsbezirk_id = rb.id
+		JOIN stimmkreis_wahlberechtigte skw ON sk.id = skw.stimmkreis_id 
+			AND skw.wahl_id = w.id
+	GROUP BY rb.id, rb."name", w.datum
+);
+
 CREATE TABLE IF NOT EXISTS "landtagswahlen".erststimmen (
 	stimmkreis_id int NOT NULL,
 	kandidat_id int NOT NULL,
 	wahl_id int NOT NULL,
-	anzahl int NOT NULL DEFAULT 0,
+	anzahlGueltige int NOT NULL DEFAULT 0,
+	anzahlUngueltige int NOT NULL DEFAULT 0,
 	FOREIGN KEY (stimmkreis_id) REFERENCES stimmkreise(id),
 	FOREIGN KEY (kandidat_id) REFERENCES kandidaten(id),
 	FOREIGN KEY (wahl_id) REFERENCES wahlen(id),
-	PRIMARY KEY (stimmkreis_id, kandidat_id, wahl_id)
+	PRIMARY KEY (stimmkreis_id, kandidat_id, wahl_id),
 );
 
 CREATE TABLE IF NOT EXISTS "landtagswahlen".kandidatgebundene_zweitstimmen (
 	stimmkreis_id int NOT NULL,
 	wahl_id int NOT NULL,
 	kandidat_id int NOT NULL,
-	anzahl int NOT NULL DEFAULT 0,		
+	anzahlGueltige int NOT NULL DEFAULT 0,
+	anzahlUngueltige int NOT NULL DEFAULT 0,
 	FOREIGN KEY (stimmkreis_id) REFERENCES stimmkreise(id),
 	FOREIGN KEY (kandidat_id) REFERENCES kandidaten(id), -- Eigentlich nur die Listenkandidaten (extra tracken?)
 	FOREIGN KEY (wahl_id) REFERENCES wahlen(id),
@@ -73,10 +116,31 @@ CREATE TABLE IF NOT EXISTS "landtagswahlen".kandidatgebundene_zweitstimmen (
 CREATE TABLE IF NOT EXISTS "landtagswahlen".listengebundene_zweitstimmen (
 	stimmkreis_id int NOT NULL,
 	wahl_id int NOT NULL,
-	partei_id int NOT NULL, -- Eigentlich Listen
-	anzahl int NOT NULL DEFAULT 0,		
+	partei_id int NOT NULL, -- Eigentlich Listen, welche aber (noch) nicht modeliert sind
+	anzahlGueltige int NOT NULL DEFAULT 0,
+	anzahlUngueltige int NOT NULL DEFAULT 0,
 	FOREIGN KEY (stimmkreis_id) REFERENCES stimmkreise(id),
 	FOREIGN KEY (partei_id) REFERENCES parteien(id),
 	FOREIGN KEY (wahl_id) REFERENCES wahlen(id),
 	PRIMARY KEY (stimmkreis_id, kandidat_id, wahl_id)
 );
+
+-- TODO: Mandatsberechnungsfkt implementieren
+--DROP MATERIALIZED VIEW "landtagswahlen".direktmandate;
+--CREATE MATERIALIZED VIEW "landtagswahlen".direktmandate AS (
+--	SELECT id
+--	FROM kandidaten
+--	LIMIT 1
+--);
+--DROP MATERIALIZED VIEW "landtagswahlen".listenmandate;
+--CREATE MATERIALIZED VIEW "landtagswahlen".listenmandate AS (
+--	SELECT id
+--	FROM kandidaten
+--	LIMIT 1
+--);
+--DROP MATERIALIZED VIEW "landtagswahlen".ausgleichsmandate;
+--CREATE MATERIALIZED VIEW "landtagswahlen".ausgleichsmandate AS (
+--	SELECT id
+--	FROM kandidaten
+--	LIMIT 1
+--);
