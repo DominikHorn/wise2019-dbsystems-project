9495d30fbdb6d70784e618bbc5f12662031bfaa7
Dominik Horn <dominik.horn@tum.de>
2019-11-25 23:22:23 +0100

    Implement wahl selector for Ergebnispage

--- implementation/src/auswertungsclient/ui/components/general/WahlSelector.tsx
+++ implementation/src/auswertungsclient/ui/components/general/WahlSelector.tsx
@@ -1,6 +1,37 @@
 import * as React from "react";
-import { Select } from "antd";
+import { Select, Spin } from "antd";
+import { IWahl } from "../../../../shared/sharedTypes";
 
-export interface IWahlSelectorProps {}
+export interface IWahlSelectorProps {
+  selectableWahlen?: IWahl[];
+  displayLoading?: boolean;
+  selectedWahl?: IWahl;
+  onSelectWahl?: (wahl: IWahl) => void;
+}
 
-export const WahlSelector = (props: IWahlSelectorProps) => <Select />;
+export const WahlSelector = (props: IWahlSelectorProps) =>
+  (props.selectableWahlen && (
+    <Select
+      loading={props.displayLoading}
+      placeholder={"Datum einer Landtagswahl"}
+      style={{ minWidth: "250px" }}
+      onSelect={(wahlidstr: string) =>
+        props.onSelectWahl &&
+        props.onSelectWahl(
+          props.selectableWahlen.find(wahl => wahl.id === Number(wahlidstr))
+        )
+      }
+      value={props.selectedWahl && props.selectedWahl.id.toString()}
+    >
+      {props.selectableWahlen.map(wahl => (
+        <Select.Option key={`${wahl.id}`}>
+          {wahl.wahldatum.toLocaleDateString("de-DE", {
+            weekday: "long",
+            year: "numeric",
+            month: "long",
+            day: "numeric"
+          })}
+        </Select.Option>
+      ))}
+    </Select>
+  )) || <Spin />;
--- implementation/src/auswertungsclient/ui/components/roots/ErgebnissePage.tsx
+++ implementation/src/auswertungsclient/ui/components/roots/ErgebnissePage.tsx
@@ -7,6 +7,9 @@ import {
   IGetAllWahlenQueryHocProps,
   withAllWahlenQuery
 } from "../../../../client-graphql/public/getAllWahlenQuery";
+import { withErrorBoundary } from "../general/ErrorBoundary";
+import { IWahl } from "../../../../shared/sharedTypes";
+import { renderInfo } from "../../guiUtil";
 
 export interface IErgebnissePageProps {
   routeProps: RouteComponentProps<any>;
@@ -14,25 +17,59 @@ export interface IErgebnissePageProps {
 
 interface IProps extends IErgebnissePageProps, IGetAllWahlenQueryHocProps {}
 
-const ErgebnissePageComponent = (props: IErgebnissePageProps) => (
-  <Card
-    title={
-      <>
-        {"Ergebnisse der Landtagswahl: "}
-        <WahlSelector />
-      </>
-    }
-    style={{ minHeight: "100%" }}
-    hoverable={true}
-  >
-    <div>TEST</div>
-  </Card>
-);
+interface IState {
+  ausgewaehlteWahl?: IWahl;
+}
+
+class ErgebnissePageComponent extends React.PureComponent<IProps, IState> {
+  constructor(props: IProps) {
+    super(props);
+    this.state = {};
+  }
+
+  private onSelectWahl = (wahl: IWahl) =>
+    this.setState({
+      ausgewaehlteWahl: wahl
+    });
+
+  render() {
+    const { allWahlenData } = this.props;
+    const { ausgewaehlteWahl } = this.state;
+
+    return (
+      <Card
+        title={
+          <>
+            {"Ergebnisse der Landtagswahl: "}
+            <WahlSelector
+              selectableWahlen={allWahlenData.allWahlen}
+              selectedWahl={ausgewaehlteWahl}
+              onSelectWahl={this.onSelectWahl}
+              displayLoading={allWahlenData.loading}
+            />
+          </>
+        }
+        style={{ minHeight: "100%" }}
+        hoverable={true}
+      >
+        {ausgewaehlteWahl ? (
+          <div>Hier könnten Ihre Ergebnisse stehen</div>
+        ) : (
+          renderInfo("Bitte eine Landtagswahl auswählen")
+        )}
+      </Card>
+    );
+  }
+}
 
 const ErgebnissePageComponentWithQueries = compose(withAllWahlenQuery())(
   ErgebnissePageComponent
 );
 
-export const ErgebnissePage = ErgebnissePageComponentWithQueries as React.ComponentType<
+const ErgebnissePageWithErrorBoundary = withErrorBoundary<IErgebnissePageProps>(
+  ErgebnissePageComponentWithQueries
+);
+
+export const ErgebnissePage = ErgebnissePageWithErrorBoundary as React.ComponentType<
   IErgebnissePageProps
 >;
