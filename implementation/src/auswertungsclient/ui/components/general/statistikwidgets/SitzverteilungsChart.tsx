42ac08fb6d2359a9f03d15aa12f342fd4af5dd2f
Dominik Horn <dominik.horn@tum.de>
2019-11-27 13:13:34 +0100

    Sort piechart slices by party name
    This makes it easier to compare different piecharts

--- implementation/src/auswertungsclient/ui/components/general/statistikwidgets/SitzverteilungsChart.tsx
+++ implementation/src/auswertungsclient/ui/components/general/statistikwidgets/SitzverteilungsChart.tsx
@@ -32,8 +32,8 @@ function aggregateMandate(
       itemStyle: { color: getParteiColor(parteiname) }
     }))
     .sort((a, b) => {
-      if (a.value < b.value) return -1;
-      if (a.value > b.value) return 1;
+      if (a.name < b.name) return -1;
+      if (a.name > b.name) return 1;
       return 0;
     });
 }
