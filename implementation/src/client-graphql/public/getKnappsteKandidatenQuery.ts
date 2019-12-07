2fde737871a1b50a255fcfb11a7855399fe906bb
Dominik Horn <dominik.horn@tum.de>
2019-12-07 21:46:49 +0100

    Add KnappsteKandidaten typed hoc query wrapper

---
+++ implementation/src/client-graphql/public/getKnappsteKandidatenQuery.ts
@@ -0,0 +1,70 @@
+import gql from "graphql-tag";
+import { IGraphqlType, createTypedGraphqlHoc } from "../typedGraphql";
+import { IKnapperKandidat } from "../../shared/sharedTypes";
+import { DataValue } from "react-apollo";
+
+const getKnappsteKandidatenQuery = gql`
+  query knappsteKandidatenQuery($wahlid: Int!, $amountPerPartei: Int) {
+    knappsteKandidaten: getKnappsteKandidaten(
+      wahlid: $wahlid
+      amountPerPartei: $amountPerPartei
+    ) {
+      wahl {
+        id
+        wahldatum
+      }
+      stimmkreis {
+        id
+        name
+      }
+      kandidat {
+        id
+        name
+        partei {
+          id
+          name
+        }
+      }
+      differenz
+      gewinner
+      platz
+    }
+  }
+`;
+
+interface IGetKnappsteKandidatenQueryResponse extends IGraphqlType {
+  readonly knappsteKandidaten?: IKnapperKandidat[];
+}
+
+interface IGetKnappsteKandidatenQueryVariables {
+  readonly wahlid: number;
+  readonly amountPerPartei: number;
+}
+
+export interface IGetKnappsteKandidatenQueryHocProps {
+  readonly knappsteKandidatenData: DataValue<
+    IGetKnappsteKandidatenQueryResponse,
+    IGetKnappsteKandidatenQueryVariables
+  >;
+}
+
+const knappsteKandidatenTypedHoc = createTypedGraphqlHoc<
+  IGetKnappsteKandidatenQueryResponse,
+  IGetKnappsteKandidatenQueryVariables
+>(getKnappsteKandidatenQuery);
+
+export const withKnappsteKandidatenQuery = <TProps = {}>(
+  getWahlId: (props: TProps) => number,
+  getAmountPerPartei?: (props: TProps) => number
+) =>
+  knappsteKandidatenTypedHoc<TProps, IGetKnappsteKandidatenQueryHocProps>({
+    options: props => ({
+      variables: {
+        wahlid: getWahlId(props),
+        amountPerPartei: getAmountPerPartei && getAmountPerPartei(props)
+      }
+    }),
+    props: ({ data }) => ({
+      knappsteKandidatenData: data
+    })
+  });
