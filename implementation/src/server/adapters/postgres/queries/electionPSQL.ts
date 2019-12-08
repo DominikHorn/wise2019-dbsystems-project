2ecfd9b405c97411a971f1c9e5c7550437ca018a
Dominik Horn <dominik.horn@tum.de>
2019-12-08 15:45:36 +0100

    Fix Ueberhangmandat query reporting wrong results

--- implementation/src/server/adapters/postgres/queries/electionPSQL.ts
+++ implementation/src/server/adapters/postgres/queries/electionPSQL.ts
@@ -22,7 +22,8 @@ type MaterialViews =
 
 const KANDIDATENGEBUNDENE_GUELTIGE_STIMMEN_MVIEW: MaterialViews =
   "kandidatgebundene_gueltige_stimmen";
-const GEWONNENE_DIREKTMANDATE: MaterialViews = "gewonnene_direktmandate";
+const GEWONNENE_DIREKTMANDATE_MVIEW: MaterialViews = "gewonnene_direktmandate";
+const GEWONNENE_LISTENMANDATE_MVIEW: MaterialViews = "gewonnene_listenmandate";
 
 type Tables =
   | "parteien"
@@ -32,7 +33,7 @@ type Tables =
   | "regierungsbezirke"
   | "direktkandidaten";
 
-const PARTEI_TABLE: Tables = "parteien";
+const PARTEIEN_TABLE: Tables = "parteien";
 const WAHL_TABLE: Tables = "wahlen";
 const STIMMKREIS_TABLE: Tables = "stimmkreise";
 const KANDIDATEN_TABLE: Tables = "kandidaten";
@@ -88,7 +89,7 @@ export async function computeWinnerParties(
         ? EWinnerPartyViews.SIEGER_ERSTSTIMMEN
         : EWinnerPartyViews.SIEGER_ZWEITSTIMMEN
     } seps
-        JOIN "${DatabaseSchemaGroup}".${PARTEI_TABLE} p on seps.partei_id = p.id
+        JOIN "${DatabaseSchemaGroup}".${PARTEIEN_TABLE} p on seps.partei_id = p.id
         JOIN "${DatabaseSchemaGroup}".${STIMMKREIS_TABLE} sk on seps.stimmkreis_id = sk.id
       WHERE seps.wahl_id = $1
     `,
@@ -108,8 +109,6 @@ export async function computeWinnerParties(
 }
 
 export async function getMandate(wahlid: number): Promise<IMandat[]> {
-  const direktmandatView: MaterialViews = "gewonnene_direktmandate";
-  const listenmandatView: MaterialViews = "gewonnene_listenmandate";
   const res: {
     kandidat_id: number;
     kandidat_name: string;
@@ -125,9 +124,9 @@ export async function getMandate(wahlid: number): Promise<IMandat[]> {
             p.id as partei_id, 
             p.name as partei_name,
             true as direktmandat
-      FROM "${DatabaseSchemaGroup}".${direktmandatView} dm
+      FROM "${DatabaseSchemaGroup}".${GEWONNENE_DIREKTMANDATE_MVIEW} dm
         JOIN "${DatabaseSchemaGroup}".${KANDIDATEN_TABLE} k ON dm.kandidat_id = k.id
-        JOIN "${DatabaseSchemaGroup}".${PARTEI_TABLE} p ON k.partei_id = p.id
+        JOIN "${DatabaseSchemaGroup}".${PARTEIEN_TABLE} p ON k.partei_id = p.id
       WHERE dm.wahl_id = $1
       UNION
       SELECT k.id as kandidat_id, 
@@ -135,9 +134,9 @@ export async function getMandate(wahlid: number): Promise<IMandat[]> {
             p.id as partei_id, 
             p.name as partei_name,
             false as direktmandat
-      FROM "${DatabaseSchemaGroup}".${listenmandatView} lm
+      FROM "${DatabaseSchemaGroup}".${GEWONNENE_LISTENMANDATE_MVIEW} lm
         JOIN "${DatabaseSchemaGroup}".${KANDIDATEN_TABLE} k ON lm.kandidat_id = k.id
-        JOIN "${DatabaseSchemaGroup}".${PARTEI_TABLE} p ON k.partei_id = p.id
+        JOIN "${DatabaseSchemaGroup}".${PARTEIEN_TABLE} p ON k.partei_id = p.id
       WHERE lm.wahl_id = $1
     ) m
     ORDER BY m.direktmandat DESC, m.kandidat_id
@@ -161,8 +160,6 @@ export async function getMandate(wahlid: number): Promise<IMandat[]> {
 export async function getUeberhangmandate(
   wahlid: number
 ): Promise<IUeberhangMandat[]> {
-  const direktmandatView: MaterialViews = "gewonnene_direktmandate";
-  const listenmandatView: MaterialViews = "gewonnene_listenmandate";
   const res: {
     wahl_id: number;
     wahldatum: Date;
@@ -177,45 +174,39 @@ export async function getUeberhangmandate(
     `
     WITH anzahlGewonneneDirektmandate (regierungsbezirk_id, partei_id, anzahl) AS (
       SELECT sk.regierungsbezirk_id, k.partei_id, count(*) as anzahl
-      FROM "${DatabaseSchemaGroup}".${direktmandatView} gd
+      FROM "${DatabaseSchemaGroup}".${GEWONNENE_DIREKTMANDATE_MVIEW} gd
         JOIN "${DatabaseSchemaGroup}".${KANDIDATEN_TABLE} k ON k.id = gd.kandidat_id
         JOIN "${DatabaseSchemaGroup}".${STIMMKREIS_TABLE} sk ON sk.id = gd.stimmkreis_id
       WHERE gd.wahl_id = $1
       GROUP BY sk.regierungsbezirk_id, k.partei_id
     ), anzahlGewonneneListenmandate (regierungsbezirk_id, partei_id, anzahl) AS (
       SELECT gl.regierungsbezirk_id, k.partei_id, count(*) as anzahl
-      FROM "${DatabaseSchemaGroup}".${listenmandatView} gl
+      FROM "${DatabaseSchemaGroup}".${GEWONNENE_LISTENMANDATE_MVIEW} gl
         JOIN "${DatabaseSchemaGroup}".${KANDIDATEN_TABLE} k ON k.id = gl.kandidat_id
       WHERE gl.wahl_id = $1
       GROUP BY gl.regierungsbezirk_id, k.partei_id
     )
-    SELECT 
-      zm.regierungsbezirk_id,
-      rb.name as regierungsbezirk_name,
-      zm.partei_id,
-      p.name as partei_name,
-      CASE
-        WHEN COALESCE(agd.anzahl, 0) - zm.anzahl < 0 THEN 0
-        ELSE COALESCE(agd.anzahl, 0) - zm.anzahl
-        END as ueberhang,
-      CASE
-        WHEN COALESCE(agl.anzahl, 0) - zm.anzahl < 0 THEN 0
-        ELSE COALESCE(agl.anzahl, 0) - zm.anzahl
-        END as ausgleich,
-      zm.anzahl as zustehend
+    SELECT COALESCE(agd.regierungsbezirk_id, agl.regierungsbezirk_id) as regierungsbezirk_id,
+           rb.name as regierungsbezirk_name,
+           COALESCE(agd.partei_id, agl.partei_id) as partei_id,
+           p.name as partei_name,
+           CASE WHEN COALESCE(agd.anzahl, 0) > zm.anzahl THEN COALESCE(agd.anzahl, 0) - zm.anzahl ELSE 0 END as ueberhang,
+           COALESCE(agd.anzahl,0) + COALESCE(agl.anzahl,0) - zm.anzahl - (
+               CASE WHEN COALESCE(agd.anzahl, 0) > zm.anzahl THEN COALESCE(agd.anzahl, 0) - zm.anzahl ELSE 0 END
+               ) as ausgleich,
+           zm.anzahl as zustehend
     FROM anzahlGewonneneDirektmandate agd
-      RIGHT OUTER JOIN zustehende_mandate(0) zm
-        ON zm.regierungsbezirk_id = agd.regierungsbezirk_id
-          AND zm.partei_id = agd.partei_id
-      LEFT OUTER JOIN anzahlGewonneneListenmandate agl
-        ON zm.regierungsbezirk_id = agl.regierungsbezirk_id
-          AND zm.partei_id = agl.partei_id
-      JOIN "${DatabaseSchemaGroup}".${REGIERUNGSBEZIRKE_TABLE} rb
-        ON rb.id = zm.regierungsbezirk_id
-      JOIN "${DatabaseSchemaGroup}".${PARTEI_TABLE} p
-        ON p.id = zm.partei_id
-    WHERE zm.wahl_id = $1
-    ORDER BY zm.regierungsbezirk_id, zm.partei_id; 
+        FULL OUTER JOIN anzahlGewonneneListenmandate agl
+          ON agd.regierungsbezirk_id = agl.regierungsbezirk_id
+            AND agd.partei_id = agl.partei_id
+        JOIN zustehende_mandate(0) zm
+          ON zm.regierungsbezirk_id = COALESCE(agd.regierungsbezirk_id, agl.regierungsbezirk_id)
+            AND zm.partei_id = COALESCE(agd.partei_id, agl.partei_id)
+            AND zm.wahl_id = $1
+        JOIN "${DatabaseSchemaGroup}".${REGIERUNGSBEZIRKE_TABLE} rb
+          ON rb.id = COALESCE(agd.regierungsbezirk_id, agl.regierungsbezirk_id)
+        JOIN "${DatabaseSchemaGroup}".${PARTEIEN_TABLE} p
+          ON p.id = COALESCE(agd.partei_id, agl.partei_id);
   `,
     [wahlid]
   );
@@ -316,7 +307,7 @@ export async function getKnappsteKandidaten(
           ON dk.direktkandidat_id = k.id
       WHERE NOT EXISTS (
         SELECT *
-        FROM "${DatabaseSchemaGroup}".${GEWONNENE_DIREKTMANDATE} gd
+        FROM "${DatabaseSchemaGroup}".${GEWONNENE_DIREKTMANDATE_MVIEW} gd
           JOIN "${DatabaseSchemaGroup}".kandidaten k2
             ON gd.kandidat_id = k2.id
         WHERE dk.wahl_id = gd.wahl_id AND k.partei_id = k2.partei_id
@@ -327,7 +318,7 @@ export async function getKnappsteKandidaten(
             vds1.kandidat_id,
             gd.stimmanzahl - vds1.stimmanzahl as differenz_zu_sieger
       FROM verlierer_direktkandidaten_stimmen vds1
-        JOIN "${DatabaseSchemaGroup}".${GEWONNENE_DIREKTMANDATE} gd
+        JOIN "${DatabaseSchemaGroup}".${GEWONNENE_DIREKTMANDATE_MVIEW} gd
           ON vds1.wahl_id = gd.wahl_id
             AND vds1.stimmkreis_id = gd.stimmkreis_id
     ), looser (wahl_id, stimmkreis_id, kandidat_id, differenz) AS (
@@ -362,7 +353,7 @@ export async function getKnappsteKandidaten(
         ON l.stimmkreis_id = sk.id
       JOIN "${DatabaseSchemaGroup}".${KANDIDATEN_TABLE} k
         ON l.kandidat_id = k.id
-      JOIN "${DatabaseSchemaGroup}".${PARTEI_TABLE} p
+      JOIN "${DatabaseSchemaGroup}".${PARTEIEN_TABLE} p
         ON l.partei_id = p.id
     WHERE wahl_id = $1 AND platz <= $2
   `,
