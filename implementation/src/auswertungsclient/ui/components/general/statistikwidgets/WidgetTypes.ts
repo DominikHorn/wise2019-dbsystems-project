133f38c06ff1cc2c8a6f5021cbea37c4871e9c51
Dominik Horn <dominik.horn@tum.de>
2020-01-26 14:05:06 +0100

    Final presentation slides

--- implementation/src/auswertungsclient/ui/components/general/statistikwidgets/WidgetTypes.ts
+++ implementation/src/auswertungsclient/ui/components/general/statistikwidgets/WidgetTypes.ts
@@ -5,13 +5,13 @@ export enum WidgetType {
   SITZVERTEILUNG_PIECHART = "Sitzverteilung (Q1) - Torte",
   SITZVERTEILUNG_TABLE = "Sitzverteilung (Q1) - Tabelle",
   MANDAT_LISTE = "Mandatliste (Q2)",
-  STIMMKREIS_INFO_WAHLBETEILIGUNG = "Stimmkreisinfo (Q3) - Wahlbeteiligung",
+  STIMMKREIS_INFO_WAHLBETEILIGUNG = "Stimmkreisinfo (Q3)",
   GEWINNER_STIMMKREISE = "Stimmkreisgewinner (Q4)",
   UEBERHANGMANDATE = "Überhangmandate (Q5)",
   KNAPPSTE_KANDIDATEN = "Knappste Kandidaten (Q6)",
   STIMMKREIS_INFO_Q7 = "Stimmkreisinfos (Q7)",
   ALTERSVERTEILUNG = "Altersverteilung im Landtag (Q8)",
-  SUPER_DIREKTKANDIDATEN = "Super-Direktkandidaten (Q9"
+  SUPER_DIREKTKANDIDATEN = "Super-Direktkandidaten (Q9)"
 }
 
 export type StatistikWidgetSetting = {
